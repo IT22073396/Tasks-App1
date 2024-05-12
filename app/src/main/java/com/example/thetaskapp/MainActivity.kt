3d644d6d65d470cb2174353456e43bc698244527
Molinda Mihiranga <it22073396@my.sliit.lk>
2024-05-12 15:59:31 +0530

    added seven part

--- app/src/main/java/com/example/thetaskapp/MainActivity.kt
+++ app/src/main/java/com/example/thetaskapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.thetaskapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.thetaskapp.database.NoteDatabase
+import com.example.thetaskapp.repository.NoteRepository
+import com.example.thetaskapp.viewmodel.NoteViewModel
+import com.example.thetaskapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
     }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+
 }
